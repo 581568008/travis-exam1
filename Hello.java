126db9cab39fe9cb6e07c0fca22e19c3d55aa16b
Withun saewue <withun.sae@crru.ac.th>
2016-11-21 13:32:37 +0700

    init file

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main (String args[]){
-		System.out.println("Name Withun Saewue ");
+		System.out.println("Withun Saewue ");
 		System.out.println("581568008 ");
 	}
 }
